d3d24f7b598c152df66a947619513c3536330e19
Crowdbotics <team@crowdbotics.com>
2020-05-30 04:02:15 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Stay_Hungry
+
+admin.site.register(Stay_Hungry)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -31,3 +31,8 @@ class HomePage(models.Model):
     @property
     def field(self):
         return "body"
+
+
+class Stay_Hungry(models.Model):
+    "Generated Model"
+    stayhungry = models.BigIntegerField()
