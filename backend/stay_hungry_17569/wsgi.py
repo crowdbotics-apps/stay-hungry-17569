c3352e89cd1043a28f887d41d833aab5be0f9acf
Crowdbotics <team@crowdbotics.com>
2020-05-30 02:45:17 +0000

    Update models

--- backend/stay_hungry_17569/wsgi.py
+++ backend/stay_hungry_17569/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stay_hungry_17569.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stay_hungry_17569.settings")
 
 application = get_wsgi_application()
